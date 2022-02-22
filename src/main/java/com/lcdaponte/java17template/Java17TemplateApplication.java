87f89c6df638c910440dc600c57e1ac3e3542016
Lucas Cavalcante <lcdaponte@gmail.com>
2022-02-21 22:16:05 -0300

    Initial commit

---
+++ src/main/java/com/lcdaponte/java17template/Java17TemplateApplication.java
@@ -0,0 +1,13 @@
+package com.lcdaponte.java17template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java17TemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java17TemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lcdaponte/java17template/Java17TemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lcdaponte.java17template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java17TemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
